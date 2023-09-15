d8c07ea03d476a58d9ea9e7ec05ccc557a7c6531
isolent <a_kakharmanova@kbtu.kz>
2023-09-15 15:30:47 +0600

    commit

---
+++ 2723.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    return new Promise((res, rej) =>{
+        promise1.then((value1)=>{
+            promise2.then((value2)=>{
+                res(value1+value2)
+            })
+        })  
+    })
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
